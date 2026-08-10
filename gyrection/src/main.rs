e21c2013cfd20a0d8db9f29943d7e4e6b6f78501
Balázs Ákos <balazs.akos@student.ms.sapientia.ro>
2026-08-10 19:09:55 +0300

    Modified UI

--- gyrection/src/main.rs
+++ gyrection/src/main.rs
@@ -3,5 +3,9 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let app = AppWindow::new()?;
 
+    app.on_connect_clicked(|| {
+        println!("Connect clicked!");
+    });
+
     app.run()
 }
